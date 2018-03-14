02c5d309c917e7102de43867e2881cf2ed7519b7
RobinFritz <robin.fritz@softproject.de>
2018-03-14 21:39:55 +0100

    Initial commit + framework spelling corrections

---
+++ src/de/hska/iwii/i2/gol/GameOfLifeLogic.java
@@ -0,0 +1,20 @@
+package de.hska.iwii.i2.gol;
+
+/**
+ * Die eigentliche Spielelogik. Das Spielfeld wird hier nicht
+ * als zyklisch geschlossen betrachtet wird.
+ *
+ * @author Holger Vogelsang
+ */
+public class GameOfLifeLogic {
+
+    public void setStartGeneration(boolean[][] generation) {
+    }
+
+    public void nextGeneration() {
+    }
+
+    public boolean isCellAlive(int x, int y) {
+        return false;
+    }
+}
